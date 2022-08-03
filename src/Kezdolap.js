e80013e4312f6a5712bf341eb05a18e28cf77887
rbrt1022 <rbrt1022.it@outlook.hu>
2022-08-03 21:23:20 +0200

    Tartalommódosítás

--- src/Kezdolap.js
+++ src/Kezdolap.js
@@ -3,7 +3,7 @@ import './Kezdolap.css';
 const Kezdolap = () => {
     return ( 
         <main id="kezdolap">
-            <p className='bevezetoszoveg'>Üdv mindenkinek! <br/> Az én nevem Róbert, és most egy kis betekintést nyerhettek az életembe a hobbijaim és az érdeklődési körök bemutatásával. <br/> Remélem az oldal megtekintése után találsz közös pontot velem. Te is sok dolgot csinálsz egyszerre, vagy pedig csak egyet? </p>
+            <p className='bevezetoszoveg'>Üdv mindenkinek! <br/> Az én nevem Róbert, és most egy kis betekintést nyerhettek az életembe a hobbijaim és az érdeklődési köreim bemutatásával. <br/> Remélem az oldal megtekintése után találsz közös pontot velem. Te is sok dolgot csinálsz egyszerre, vagy pedig csak egyet? </p>
             <p className='bevezetoszoveg'>Ha te is több dolgot művelsz egyszerre, akkor biztosan itt a helyed!</p>
         </main>
      );
