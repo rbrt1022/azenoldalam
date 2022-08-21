e47feef01f4abd2842cb8763ee2e81fe28472348
rbrt1022 <rbrt1022.it@outlook.hu>
2022-08-21 21:52:56 +0200

    Vittu

--- src/Torzs.js
+++ src/Torzs.js
@@ -8,7 +8,7 @@ const Torzs = () => {
                 <h3>Weblapszerkesztés</h3>
                 <article>
                     <img className='tkep' src="https://www.gyoremihaly.hu/wp-content/uploads/2021/08/icons8-visual-studio-code-2019-480.png" alt="" />
-                    <p className='programszoveg'>Még 2018-ban, amikor nyolcadik évfolyamba jártam, akkor eljött számomra is az idő, iskolát kellett választani a továbbtanuláshoz. Én már akkor tudtam, hogy az informatika irányába szeretnék menni. Itt helyben, a <a className='linkhatternelkul' target="_blank" href='https://www.facebook.com/adybaygyszc'>Gyulai SZC Ady Endre - Bay Zoltán Technikum és Szakképző iskola</a> informatikai rendszerüzemeltető képzésére lehetett jelentkezni a régebbi szakgimnáziumi rendszerben (4+1 év). <br/>
+                    <p className='programszoveg'>Még 2018-ban, amikor nyolcadik évfolyamba jártam, akkor eljött számomra is az idő, iskolát kellett választani a továbbtanuláshoz. Én már akkor tudtam, hogy az informatika irányába szeretnék menni. Itt helyben, a <a className='linkhatternelkul' href='https://www.facebook.com/adybaygyszc' target="_blank">Gyulai SZC Ady Endre - Bay Zoltán Technikum és Szakképző iskola</a> informatikai rendszerüzemeltető képzésére lehetett jelentkezni a régebbi szakgimnáziumi rendszerben (4+1 év). <br/>
                     Részben jól döntöttem, mert a programozás órák keretein belül a weblapszerkesztést kedveltem meg.</p>
                 </article>
             </section>
@@ -16,11 +16,11 @@ const Torzs = () => {
                 <h3>Labdarúgás</h3>
                 <article className='vissza'>
                     <img className='tkep' src={KinizsiKep} alt="" />
-                    <p className='programszoveg'>Ennek a története kicsit visszább nyúlik az időben. 2013-ban kezdtem el focizni a Sarkadi USC-nél. Pár év eltöltése után egyéb okok miatt pár évre szüneteltettem. Ennek időtartama alatt a Kálvin téren játszottam hobbiból. Voltam egy évet Méhkeréken, majd 2018-ban visszatértem a Sarkadi KLE-hez. Posztjaimat tekintve univerzális vagyok, kapuban is úgy állom helyem, mint mezőnyben. <a className='link' href='https://ada1bank.mlsz.hu/player?itemId=573573'>Ide kattintva áttekintheted a pályafutásomat.</a></p>
+                    <p className='programszoveg'>Ennek a története kicsit visszább nyúlik az időben. 2013-ban kezdtem el focizni a Sarkadi USC-nél. Pár év eltöltése után egyéb okok miatt pár évre szüneteltettem. Ennek időtartama alatt a Kálvin téren játszottam hobbiból. Voltam egy évet Méhkeréken, majd 2018-ban visszatértem a Sarkadi KLE-hez. Posztjaimat tekintve univerzális vagyok, kapuban is úgy állom helyem, mint mezőnyben. <a className='link' href='https://ada1bank.mlsz.hu/player?itemId=573573' target="_blank">Ide kattintva áttekintheted a pályafutásomat.</a></p>
                 </article>
             </section>
             <section id="Forma-1">
-                <h3>Forma 1</h3>
+                <h3>Forma1</h3>
                 <article>
                     <img className='tkep' src="https://formula.hu/mainpage_thumb/6drvb5el91i163uibdza_kiemelt_fooldali.jpg" alt="" />
                     <p className='programszoveg'>A legelső emlékem a 2009-es Magyar Nagydíjról származik, amikor is a tv-ben adták, hogy Felipe Massa fejét eltalálta a Rubens Barichello autójáról lerepült rugó. 2010-ben kezdtem el komolyabban nézni, amikor már a Ferrariban ülő Fernando Alonsonak szurkoltam. Később tudtam csak meg róla a történetet a debütálásáról és a világbajnoki címeriről.</p>
